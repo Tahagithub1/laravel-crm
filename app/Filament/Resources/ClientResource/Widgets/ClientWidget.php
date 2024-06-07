29320ada27a9457d3e53b6dab01edfd34340e12e
Tahagithub1 <mogevaraluffyy@gmail.com>
2024-06-07 15:51:00 +0330

    WidgetRealDataWithTrend

--- app/Filament/Resources/ClientResource/Widgets/ClientWidget.php
+++ app/Filament/Resources/ClientResource/Widgets/ClientWidget.php
@@ -3,22 +3,38 @@
 namespace App\Filament\Resources\ClientResource\Widgets;
 
 use App\Filament\Resources\ClientResource;
+use App\Models\Client;
 use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
 
 class ClientWidget extends ChartWidget
 {
-    protected static ?string $heading = 'Client Chart';
+    protected static ?string $heading = 'Client Register Chart';
 
     protected function getData(): array
     {
+        $data = Trend::model(Client::class)
+            ->between(
+                now()->subMonth(6),
+                now()
+            )
+            ->perMonth()
+            ->count();
         return [
+//            'datasets' => [
+//                [
+//                    'label' => 'last register client',
+//                    'data' => [37 , 22 , 50 , 41 ,28 , 60]
+//                ]
+//            ],
+//            'labels' => ['May' , 'Jun' , 'Jul' , 'Aug' , 'Sep' , 'Dct']
             'datasets' => [
                 [
                     'label' => 'last register client',
-                    'data' => [37 , 22 , 50 , 41 ,28 , 60]
+                    'data' => $data->map(fn($value) => $value->aggregate)
                 ]
             ],
-            'labels' => ['May' , 'Jun' , 'Jul' , 'Aug' , 'Sep' , 'Dct']
+            'labels' => $data->map(fn($value) => $value->date)
         ];
 
     }
